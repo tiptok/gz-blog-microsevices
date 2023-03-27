6abc12c75c54f2ee2fde132525d1ccb1e26580a8
yangfu <785409885@qq.com>
2023-03-27 10:15:13 +0800

    deploy

--- app/user/cmd/rpc/user.go
+++ app/user/cmd/rpc/user.go
@@ -3,11 +3,11 @@ package main
 import (
 	"flag"
 	"fmt"
-
 	"github.com/tiptok/gz-blog-microsevices/api/protobuf/user/v1"
 	"github.com/tiptok/gz-blog-microsevices/app/user/cmd/rpc/internal/config"
 	"github.com/tiptok/gz-blog-microsevices/app/user/cmd/rpc/internal/server"
 	"github.com/tiptok/gz-blog-microsevices/app/user/cmd/rpc/internal/svc"
+	"log"
 
 	"github.com/zeromicro/go-zero/core/conf"
 	"github.com/zeromicro/go-zero/core/service"
@@ -26,13 +26,16 @@ func main() {
 	ctx := svc.NewServiceContext(c)
 	svr := server.NewUserServiceServer(ctx)
 
-	s := zrpc.MustNewServer(c.RpcServerConf, func(grpcServer *grpc.Server) {
+	s, err := zrpc.NewServer(c.RpcServerConf, func(grpcServer *grpc.Server) {
 		v1.RegisterUserServiceServer(grpcServer, svr)
 
 		if c.Mode == service.DevMode || c.Mode == service.TestMode {
 			reflection.Register(grpcServer)
 		}
 	})
+	if err != nil {
+		log.Fatal(err)
+	}
 	defer s.Stop()
 
 	fmt.Printf("Starting rpc server at %s...\n", c.ListenOn)
